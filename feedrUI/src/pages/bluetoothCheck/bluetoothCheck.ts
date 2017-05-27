3f66714b100f9e181dfcf505de730e936006cec9
kaminto <kaminto@outlook.com>
2017-05-27 22:26:47 +0300

    bluetooth test code to start the bluetooth feature

--- feedrUI/src/pages/bluetoothCheck/bluetoothCheck.ts
+++ feedrUI/src/pages/bluetoothCheck/bluetoothCheck.ts
@@ -3,21 +3,56 @@ import { NavController, NavParams } from 'ionic-angular';
 import { FeedingSchedulePage } from "../feedingSchedule/feedingSchedule";
 import { PastFeedPage } from "../pastFeed/pastFeed";
 import { FeedNowPage } from "../feedNow/feedNow";
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 @Component({
   selector: 'page-bluetoothCheck',
   templateUrl: 'bluetoothCheck.html'
 })
 export class BluetoothCheckgPage {
+  msg:any;
+  enable_button:boolean = false;
+  toggle:boolean =true;
 
-feedingSchedulePage: any;
-pastFeedPage: any;
-feedNowPage: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  this.feedingSchedulePage = FeedingSchedulePage;
-   this.pastFeedPage = PastFeedPage;
-    this.feedNowPage = FeedNowPage;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,private bluetoothSerial: BluetoothSerial) {
+ 
 
   }
+
+   checkBlueTooth(){
+    this.bluetoothSerial.isEnabled().then((data)=>{
+        this.msg = data;
+      },(err)=>{
+        this.msg = err;
+        this.enable_button = true;
+      });
+  }
+  toggleBlueTooth(){
+    if(this.toggle){
+       this.bluetoothSerial.enable().then((data)=>{
+        this.msg = data;
+      },(err)=>{
+        this.msg = err;
+      });
+      this.toggle = false;
+      
+    }
+    else{
+      this.bluetoothSerial.disconnect();
+     
+    }
+  }
+  show_settings(){
+    this.bluetoothSerial.showBluetoothSettings().then((data)=>{
+        this.msg = data;
+      },(err)=>{
+        this.msg = err;
+      });
+      
+      
+    }
+
+
 
 }
